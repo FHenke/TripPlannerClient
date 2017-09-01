97f31204134b77e7a039b06eb14d783f1e2f37c2
FHenke <florian.henke@stud.uni-goettingen.de>
2017-09-01 21:28:27 +0200

    before adding ajax to index

---
+++ json.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Florian
+ * Date: 01.09.2017
+ * Time: 21:15
+ */
+
+echo "[{\"origin\":{\"name\":\"Hannover\",\"type\":0,\"longitude\":1.7976931348623157E308,\"latitude\":1.7976931348623157E308},\"destination\":{\"name\":\"Dortmund\",\"type\":0,\"longitude\":1.7976931348623157E308,\"latitude\":1.7976931348623157E308},\"price\":0.0,\"duration\":{\"iMillis\":7851000},\"type\":0,\"direct\":false,\"subConnections\":[],\"returnConnection\":[],\"weekday\":0,\"distance\":211597},{\"origin\":{\"name\":\"Dortmund\",\"type\":0,\"longitude\":1.7976931348623157E308,\"latitude\":1.7976931348623157E308},\"destination\":{\"name\":\"Hannover\",\"type\":0,\"longitude\":1.7976931348623157E308,\"latitude\":1.7976931348623157E308},\"price\":0.0,\"duration\":{\"iMillis\":7814000},\"type\":0,\"direct\":false,\"subConnections\":[],\"returnConnection\":[],\"weekday\":0,\"distance\":211523}]";
+
+?>
